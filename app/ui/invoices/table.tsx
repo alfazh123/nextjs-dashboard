e1e6008b71d6c13828928340bafb31b88e828ef5
alfazh123 <ahmdmufahras.22020@mhs.unesa.ac.id>
2023-11-22 12:50:54 +0700

    fixing alt

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -29,6 +29,7 @@ export default async function InvoicesTable({
                       <Image
                         src={invoice.image_url}
                         className="mr-2 rounded-full"
+                        alt='Customer profile picture'
                         width={28}
                         height={28}
                       />
@@ -89,6 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt={`${invoice.name}'s profile picture`}
                       />
                       <p>{invoice.name}</p>
                     </div>
